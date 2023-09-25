f9158500cf4a9777670dc3a13d23f1ae9bae23e7
Henrique de Oliveira <henriqueoliwork@gmail.com>
2023-09-25 14:44:44 -0300

    refactor: remove nuxt-kit import

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,6 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-import { createResolver } from 'nuxt/kit'
-
-const { resolve } = createResolver(import.meta.url)
-
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxt/image'],
-  css: [resolve('assets/css/tailwind.css'), resolve('assets/css/main.css')],
+  css: ['./assets/css/tailwind.css', './assets/css/main.css'],
   vue: {
     defineModel: true
   },
